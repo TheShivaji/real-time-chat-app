0f3ddbd1f78628fb5ee94ba757d2a3c76273950f
TheShivaji <shivajijagdale2005@gmail.com>
2026-03-06 19:30:42 +0530

    implement users sidebar with dynamic selection

--- Frontend/Chat/src/pages/HomePage.jsx
+++ Frontend/Chat/src/pages/HomePage.jsx
@@ -1,19 +1,19 @@
-import { useChatStore } from "../store/useChatStore"; // 🔥 Tera store import kiya
+import { useChatStore } from "../store/useChatStore"; 
 import NoChatSelected from "../components/Nochatseleced";
-import ChatContainer from "../components/ChatContaine"; // Apne file ka naam match kar lena
+import ChatContainer from "../components/ChatContaine";
 import Sidebar from "../components/Sidebar";
 
 const HomePage = () => {
-  // 🔥 Asli magic: Store se selectedUser nikal liya!
+
   const { selectedUser } = useChatStore();
 
   return (
     <div className="h-[calc(100vh-100px)] w-full bg-base-100 flex overflow-hidden text-base-content">
 
-      {/* Sidebar */}
+
       <Sidebar />
 
-      {/* Main Chat Area */}
+
       <div className="flex-1 bg-base-200 flex flex-col h-full overflow-hidden">
 
         {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
